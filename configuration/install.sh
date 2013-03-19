04b66887c9d42c011575b4d1702c412271143855
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2013-03-19 17:06:44 +0100

    fixes

--- configuration/install.sh
+++ configuration/install.sh
@@ -10,13 +10,13 @@ mkdir -p ${PREFIX}/stage/opt/glite/yaim/examples/siteinfo/services/
 mkdir -p ${PREFIX}/stage/opt/glite/yaim/functions
 mkdir -p ${PREFIX}/stage/opt/glite/share/man/man1
 mkdir -p ${PREFIX}/stage/opt/glite/yaim/node-info.d
-mkdir -p ${PREFIX}/usr/libexec
-mkdir -p ${PREFIX}/etc/glite-wms
-mkdir -p ${PREFIX}/usr/share/doc/glite-wms-configuration-${VERSION}
+mkdir -p ${PREFIX}/stage/usr/libexec
+mkdir -p ${PREFIX}/stage/etc/glite-wms
+mkdir -p ${PREFIX}/stage/usr/share/doc/glite-wms-configuration-${VERSION}
 
-cp config/glite-wms-check-daemons.sh.in ${PREFIX}/usr/libexec/glite-wms-check-daemons.sh
-cp config/glite-wms-parse-configuration.sh.in ${PREFIX}/usr/libexec/glite-wms-parse-configuration.sh
-cp config/glite-wms-services-certs.sh ${PREFIX}/usr/libexec/glite-wms-services-certs.sh
+cp config/glite-wms-check-daemons.sh.in ${PREFIX}/stage/usr/libexec/glite-wms-check-daemons.sh
+cp config/glite-wms-parse-configuration.sh.in ${PREFIX}/stage/usr/libexec/glite-wms-parse-configuration.sh
+cp config/glite-wms-services-certs.sh ${PREFIX}/stage/usr/libexec/glite-wms-services-certs.sh
 
 cp config/defaults/* ${PREFIX}/stage/opt/glite/yaim/defaults/
 gzip config/man/glite-WMS.1
@@ -25,11 +25,11 @@ cp config/functions/* ${PREFIX}/stage/opt/glite/yaim/functions/
 cp config/services/glite-wms ${PREFIX}/stage/opt/glite/yaim/services/
 cp config/node-info.d/* ${PREFIX}/stage/opt/glite/yaim/node-info.d/
 
-chmod a+x ${PREFIX}/usr/libexec/glite-wms-check-daemons.sh
-chmod a+x ${PREFIX}/usr/libexec/glite-wms-parse-configuration.sh
-chmod a+x ${PREFIX}/usr/libexec/glite-wms-services-certs.sh
+chmod a+x ${PREFIX}/stage/usr/libexec/glite-wms-check-daemons.sh
+chmod a+x ${PREFIX}/stage/usr/libexec/glite-wms-parse-configuration.sh
+chmod a+x ${PREFIX}/stage/usr/libexec/glite-wms-services-certs.sh
 
-cp config/glite_wms.conf ${PREFIX}/etc/glite-wms/wms.conf.template
+cp config/glite_wms.conf ${PREFIX}/stage/etc/glite-wms/wms.conf.template
 
-cp LICENSE ${PREFIX}/usr/share/doc/glite-wms-configuration-${VERSION}
-chmod a-x ${PREFIX}/usr/share/doc/glite-wms-configuration-${VERSION}/LICENSE
+#cp LICENSE ${PREFIX}/stage/usr/share/doc/glite-wms-configuration-${VERSION}
+#chmod a-x ${PREFIX}/stage/usr/share/doc/glite-wms-configuration-${VERSION}/LICENSE
